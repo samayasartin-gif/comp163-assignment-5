1dcb91c4da39bf6dcb141c0e614c1b8c559a9625
samayasartin-gif <samayasartin@gmail.com>
2025-09-30 14:02:00 -0400

    Challenge 1: Collatz sequence
    while loop for unknown iterations

---
+++ samaya_assignment_5.py
@@ -0,0 +1,12 @@
+current_number = int(input("Enter starting number: "))
+step_count = 0
+print("Sequence", end=" ")
+while (current_number != 1):
+    print( current_number, end=" ")
+    if current_number % 2 == 0:
+        current_number//=2
+    elif current_number % 2 != 0:
+        current_number = (current_number * 3) +1
+    step_count += 1
+print("1")
+print(f"Steps: {step_count}")
